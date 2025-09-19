aee7fe82192ac2ab2ffbfce7a00e8433bf6f1cc7
Carlos Rico <carlos.rico.salazar@gmail.com>
2025-09-18 20:01:37 -0500

    Added material theme

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatChipsModule } from '@angular/material/chips';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatToolbarModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    MatChipsModule
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ src/app/common/common.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule as AngularCommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    AngularCommonModule
+  ],
+  exports: [
+  ],
+  providers: [
+  ]
+})
+export class CommonModule { }
