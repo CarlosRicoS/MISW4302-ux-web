52359f25bb9783da6557dfcef5666efa6f9e121b
Angel Henao <ay.henao@uniandes.edu.co>
2025-09-20 12:01:12 -0500

    Added loading view

---
+++ src/app/views/components/loading-view/loading-view.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loading-view',
+  templateUrl: './loading-view.html',
+  styleUrl: './loading-view.scss',
+  standalone: false,
+})
+export class LoadingView {
+
+}
