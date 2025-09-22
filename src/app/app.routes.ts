29a1b09f565c9ea8cd3a5a804c245a9014cfe0b5
Angel Henao <ay.henao@uniandes.edu.co>
2025-09-21 20:01:12 -0500

    Added progress view

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginView } from './views/components/login-view/login-view';
 import { LoadingView } from './views/components/loading-view/loading-view';
 import { ProgramView } from './views/components/program-view/program-view';
+import { ProgressView } from './views/components/progress-view/progress-view';
 
 export const routes: Routes = [
   {
@@ -15,5 +16,9 @@ export const routes: Routes = [
   {
     path: 'program',
     component: ProgramView
+  },
+  {
+    path: 'progress',
+    component: ProgressView
   }
 ];
