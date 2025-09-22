c91d4c61fff22034e83a14fd2f318e8e8f4e9304
Carlos Rico <carlos.rico.salazar@gmail.com>
2025-09-21 23:15:23 -0500

    Added program view

---
+++ src/app/views/components/program-view/program-view.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-program-view',
+  templateUrl: './program-view.html',
+  styleUrl: './program-view.scss',
+  standalone: false,
+})
+export class ProgramView {
+
+}
--- src/app/views/views-module.ts
+++ src/app/views/views-module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { LoginView } from './components/login-view/login-view';
 import { CommonModule } from '../common/common.module';
 import { LoadingView } from './components/loading-view/loading-view';
+import { ProgramView } from './components/program-view/program-view';
 
 
 
@@ -9,6 +10,7 @@ import { LoadingView } from './components/loading-view/loading-view';
   declarations: [
     LoginView,
     LoadingView,
+    ProgramView,
   ],
   imports: [
     CommonModule,
@@ -16,6 +18,7 @@ import { LoadingView } from './components/loading-view/loading-view';
   exports: [
     LoginView,
     LoadingView,
+    ProgramView,
   ]
 })
 export class ViewsModule { }
